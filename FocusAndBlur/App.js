27ff08691ab065f3f17fe20eb2b70c6b12aeebbb
Shiva-0515 <shivakanchanapelli24@gmail.com>
2025-04-23 10:41:35 +0530

    Focus And Blur

---
+++ FocusAndBlur/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import FocusBlurDemo from './Components/FocusAndBlur';
+
+function App() {
+  return (
+    <div>
+    
+      <FocusBlurDemo />
+     
+    </div>
+  );
+}
+
+export default App;
